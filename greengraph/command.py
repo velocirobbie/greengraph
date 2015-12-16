83224b761ac183fd163e779025b3b557d1afff67
Robert Sinclair <rcsinclair3@gmail.com>
2015-12-16 11:00:18 +0000

    did some more argparse

--- greengraph/command.py
+++ greengraph/command.py
@@ -1,20 +1,20 @@
 from graph import Greengraph
 from argparse import ArgumentParser
-import pyplot as plt
+from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = 'Plot the propotion of green space between two locations')
     
-    parser.add_argument('--start','-s')
-    parser.add_argument('--end','-e')
-    parser.add_argument('--steps','-n')
-    parser.add_argument('--out','-o')
+    parser.add_argument('--start','-s',type='str',required=True)
+    parser.add_argument('--end','-e',type='str',required=True)
+    parser.add_argument('--steps','-n',default=10,type='int')
+    parser.add_argument('--out','-o',default='green_between.png')
 
     arguments = parser.parse_args()
 
 
 
-    graph = Greengraph(arguments.start,arguments.end)
+    mygraph = Greengraph(arguments.start,arguments.end)
     data = mygraph.green_between(arguments.steps)
     plt.plot(data)
     plt.savefig(arguments.out)
