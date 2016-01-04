d5bdebdfb294f1d519a7c106154691245a292376
Robert Sinclair <rcsinclair3@gmail.com>
2016-01-04 11:41:53 +0000

    added help messages

--- greengraph/command.py
+++ greengraph/command.py
@@ -5,10 +5,14 @@ from matplotlib import pyplot as plt
 def process():
     parser = ArgumentParser(description = 'Plot the propotion of green space between two locations')
     
-    parser.add_argument('--start','-s',type=str,required=True)
-    parser.add_argument('--end','-e',type=str,required=True)
-    parser.add_argument('--steps','-n',default=10,type=int)
-    parser.add_argument('--out','-o',default='green_between.png')
+    parser.add_argument('--start','-s',type=str,required=True,
+        help='The starting location')
+    parser.add_argument('--end','-e',type=str,required=True,
+        help='Then end location')
+    parser.add_argument('--steps','-n',default=10,type=int,
+        help='Number of satalite images to inspect between start and end locations - default = 10')
+    parser.add_argument('--out','-o',default='green_between.png',
+        help='Name of the .png output file the calculated graph will be written to -  default = green_between.png')
 
     arguments = parser.parse_args()
 
