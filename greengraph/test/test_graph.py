95d9f92b9a61df3987491c7ea06656352d824388
Robert Sinclair <rcsinclair3@gmail.com>
2016-01-09 17:44:31 +0000

    corrected map call test

---
+++ greengraph/test/test_graph.py
@@ -0,0 +1,12 @@
+from greengraph.graph import Greengraph
+from mock import Mock, patch
+import geopy
+from nose.tools import assert_equal
+
+def test_graph_init():
+    with patch.object(geopy.geocoders,'GoogleV3') as mock_get:
+        trial_graph = Greengraph('London','Manchester')
+        assert_equal(trial_graph.start, 'London')
+        assert_equal(trial_graph.end, 'Manchester')
+        mock_get.assert_called_with(domain="maps.google.co.uk")
+
--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -1,22 +1,42 @@
 import numpy as np
-from StringIO import StrinIO
+from StringIO import StringIO
 from matplotlib import image as img
 import requests
-from nose.tools import assert_raises
-from map import Map
+from nose.tools import assert_equal, assert_raises
+from greengraph.map import Map
 from mock import Mock, patch
 
-def test_build_map_params():
+@patch('matplotlib.image.imread')
+@patch('StringIO.StringIO')
+def test_build_map_params(mock_img,mock_string):
     with patch.object(requests,'get') as mock_get:
-        default_map = map_at(51.0,0.0)
+        trial_map = Map(50.0,50.0)
         mock_get.assert_called_with(
         "http://maps.googleapis.com/maps/api/staticmap?",
         params={
-            'senson':'false',
-            'zoom':12,
+            'sensor':'false',
+            'zoom':10,
             'size':'400x400',
-            'center':'51.0,0.0'
-            'style':'feature:all|element:labels|visibility:off'
+            'center':'50.0,50.0',
+            'style':'feature:all|element:labels|visibility:off',
+            'maptype':'satellite'
             }
         )
-        test_build_default_params()
+
+
+
+
+#def test_green_logical_array():
+#    test_array = np.zeros((2,2,3))
+#    test_array[0,0,:] = 0.5, 0, 1
+#    test_array[0,1,:] = 0.5, 1, 0
+#    test_array[1,0,:] = 1, 0.5, 0.5
+#    test_array[1,1,:] = 1, 0.5, 0
+#    test_map = Map(test_array
+#    
+#    test_logical_array = np.zeros((2, 2), dtype=bool)
+#    test_logical_array[1,:] = True, True
+#
+#    assert_equal(Map.green(test_array,1.1),test_logical_array)
+#
+
