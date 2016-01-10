6aa7b63598dee33cbc42fde80a584bc4a2d5c10e
Robert Sinclair <rcsinclair3@gmail.com>
2016-01-10 20:18:36 +0000

    added count_green test + fixtures in green.yaml

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -28,34 +28,32 @@ def test_build_map_params(mock_img,mock_string):
         )
 
 
-#@patch('matplotlib.image.imread')
 @patch('StringIO.StringIO')
 def test_green(mock_string):
     with patch.object(matplotlib.image,'imread') as mock_get:
         with open(os.path.join(os.path.dirname(__file__),'fixtures','green.yaml')) as fixtures_file:
             fixtures=yaml.load(fixtures_file)
             for fixture in fixtures:
-
                 mock_get.return_value = np.array(fixture.pop('pixles'))
                 expected_green = fixture.pop('answer')
                 trial_map = Map('junk','junk')
                 actual_green = trial_map.green(1)
+                # cannot assert_equal on arrays, instead loop over elements
                 for x in range(2):
                     for y in range(2):
                         assert_equal(expected_green[x][y],actual_green[x][y])
 
+@patch('StringIO.StringIO')
+def test_count_green(mock_string):
+    with patch.object(matplotlib.image,'imread') as mock_get:
+        with open(os.path.join(os.path.dirname(__file__),'fixtures','green.yaml')) as fixtures_file:
+            fixtures=yaml.load(fixtures_file)
+            for fixture in fixtures:
+                mock_get.return_value = np.array(fixture.pop('pixles'))
+                expected_count = fixture.pop('count_answer')
+                trial_map = Map('junk','junk')
+                actual_count = trial_map.count_green(1)
+                assert_equal(expected_count,actual_count)
 
-#def test_green_logical_array():
-#    test_array = np.zeros((2,2,3))
-#    test_array[0,0,:] = 0.5, 0, 1
-#    test_array[0,1,:] = 0.5, 1, 0
-#    test_array[1,0,:] = 1, 0.5, 0.5
-#    test_array[1,1,:] = 1, 0.5, 0
-#    test_map = Map(test_array
-#    
-#    test_logical_array = np.zeros((2, 2), dtype=bool)
-#    test_logical_array[1,:] = True, True
-#
-#    assert_equal(Map.green(test_array,1.1),test_logical_array)
-#
+   
 
