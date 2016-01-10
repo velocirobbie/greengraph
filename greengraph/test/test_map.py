b8067bd2daaf2ea8014ab6a3f84df3fff3a8cf22
Robert Sinclair <rcsinclair3@gmail.com>
2016-01-10 19:43:37 +0000

    start addiing map.green test with patch, problem creating an array

--- greengraph/test/test_map.py
+++ greengraph/test/test_map.py
@@ -24,6 +24,25 @@ def test_build_map_params(mock_img,mock_string):
         )
 
 
+@patch('matplotlib.image.imread')
+@patch('StringIO.StringIO')
+def test_green(mock_imread,mock_string):
+#    red   = [[0,0],[1,1]]
+#    green = [[5,0],[5,0]]
+#    blue  = [[1,0],[1,0]]
+    mock_imread.return_value = np.array([[[0,5,1],[1,5,1]],
+                                         [[1,0,0],[0,0,1]]])
+#np.dstack((red,green,blue))
+    expected_green = [[True,True],[False,False]]
+
+    trial_map = Map('junk','junk')
+    print mock_imread()[:,:,1]
+    actual_green = trial_map.green(1)
+    print expected_green,actual_green
+    # cannot assert_equal on arrays, instead loop over elements
+    for x in range(2):
+        for y in range(2):
+            assert_equal(expected_green[x][y],actual_green[x][y])
 
 
 #def test_green_logical_array():
